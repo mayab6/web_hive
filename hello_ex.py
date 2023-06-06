e48e66a487fa72fdc2523f2b80bd90902f52479c
mayab6 <mayab6@mail.tau.ac.il>
2023-06-06 19:59:07 +0300

    users

--- hello_ex.py
+++ hello_ex.py
@@ -2,11 +2,15 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+@app.route('/signup')
+def signup():
+    return render_template('sign_up.html')
+
 
 @app.route('/')
 def index():
     name = request.args.get('name')
-    return render_template('hello_css.html', name=name)
+    return render_template('home.html', name=name, url=signup())
 
 
 if __name__ == '__main__':
